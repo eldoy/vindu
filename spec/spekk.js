17d7f82f257eebe48cfa1713bfa9f28e3210d12c
acmb <48370302+acmb@users.noreply.github.com>
2024-07-18 14:19:32 +0100

    Update spekk.js

--- spec/spekk.js
+++ spec/spekk.js
@@ -1,10 +1,40 @@
 var mongowave = require('mongowave')
 var vindu = require('../index.js')
 
+var _Date = Date
+Object.getOwnPropertyNames(Date).forEach(function (name) {
+  _Date[name] = Date[name]
+})
+
+function mockDate(date = new Date()) {
+  Date = function () {
+    return new _Date(date)
+  }
+
+  Object.getOwnPropertyNames(_Date).forEach(function (name) {
+    Date[name] = _Date[name]
+  })
+}
+
+function resetDate() {
+  Date = _Date
+}
+
 module.exports = async function () {
   var db = await mongowave('vindu-test')
 
-  var $ = { vindu, db, req: {} }
+  async function throttler($, opts = {}) {
+    var limit = opts.limit || 30
+    var { minute } = await vindu($, opts)
+    var requestsLastMinute = await minute()
+    return requestsLastMinute > limit
+  }
 
-  return { $ }
+  function after() {
+    resetDate()
+  }
+
+  var $ = { vindu, throttler, db, mockDate, resetDate }
+
+  return { $, after }
 }
